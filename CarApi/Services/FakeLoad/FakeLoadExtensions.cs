eebf0d6caf77cb5ce3e3bea91ae4767830e2b3cc
Jako5457 <jako5457@gmail.com>
2026-08-04 01:02:07 +0200

    Changed alot

---
+++ CarApi/Services/FakeLoad/FakeLoadExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace CarApi.Services.FakeLoad
+{
+    public static class FakeLoadExtensions
+    {
+        public static IApplicationBuilder UseFakeLoad(this IApplicationBuilder builder)
+        {
+            return builder.Use(async (context, next) =>
+            {
+                var scope = context.RequestServices.CreateScope();
+                var logger = scope.ServiceProvider.GetRequiredService<ILogger<FakeLoadMiddleWare>>();
+
+                // Instantiate and run our custom middleware logic
+                var middleware = new FakeLoadMiddleWare(next, logger);
+                await middleware.InvokeAsync(context);
+            });
+        }
+
+    }
+}
